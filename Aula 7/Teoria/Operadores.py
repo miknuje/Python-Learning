75a1399c2741c7c0cd6269457d08279d1f9b1ddd
Renato Freitas <130066564+miknuje@users.noreply.github.com>
2026-01-12 02:02:46 +0000

    Create Operadores.py

---
+++ Aula 7/Teoria/Operadores.py
@@ -0,0 +1,17 @@
+''' 
++ Adição
+- Subtração
+* Multiplicação
+/ Divisão
+% Resto
+** Potência
+// Divisão inteira
+'''
+
+print(5+2)
+print(5-2)
+print(5*2)
+print(5/2)
+print(5%2)
+print(5**2)
+print(5//2)
