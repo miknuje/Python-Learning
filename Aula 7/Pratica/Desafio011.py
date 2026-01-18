b3b4fbeb1a04092186caf7e32dc0861f6d87410e
Renato Freitas <130066564+miknuje@users.noreply.github.com>
2026-01-18 13:27:51 +0000

    desafio 11 completo

--- Aula 7/Pratica/Desafio011.py
+++ Aula 7/Pratica/Desafio011.py
@@ -4,5 +4,11 @@ parede em metros, calcule a sua área e a quantidade de
 tinta necessária para pintá-la, sabendo que cada litro 
 de tinta pinta uma área de 2m².
 '''
-largura = float(input('Escrava a largura: '))
-altura = float(input('Escrava a altura: '))
+largura = float(input('Escreva a largura: '))
+altura = float(input('Escreva a altura: '))
+
+area = largura * altura
+
+tinta = area / 2
+
+print('A quantidade de tinta é {:.2f}L.'.format(tinta))
