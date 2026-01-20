69945f01bb9c4cc1f3a7b5cc3efb21a93486b89f
Renato Freitas <130066564+miknuje@users.noreply.github.com>
2026-01-20 16:33:09 +0000

    ex5,6 da aula 7

---
+++ Exercicios/ex005.py
@@ -0,0 +1,6 @@
+numero = int(input('Escreva um numero: '))
+
+sucessor = numero + 1
+antecessor = numero - 1
+
+print('O sucessor do numero {} é {} e o antecessor é {}'.format(numero, sucessor, antecessor))
---
+++ Exercicios/ex006.py
@@ -0,0 +1,7 @@
+numero = int(input('Escreva seu número: '))
+
+dobro = numero * 2
+triplo = numero * 3
+raizQuadrada = numero  ** (1/2)
+
+print('O dobro do numero {} é {}, o triplo é {} e a raiz quadrada é {}'.format(numero, dobro, triplo, raizQuadrada))
