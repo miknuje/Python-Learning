9f5d4995a4b71c85f0c01d82ff3153aacf57528f
Renato Freitas <130066564+miknuje@users.noreply.github.com>
2026-01-23 21:43:42 +0000

    correção ex008

--- Exercicios/ex008.py
+++ Exercicios/ex008.py
@@ -3,4 +3,4 @@ metros = float(input('Quantos metros andaste? '))
 centimetros = metros * 100
 milimetros = metros * 1000
 
-print('Andaste {} centimetros ou {} milimetros que são os {} metros que digitaste.'.format(centimetros, milimetros, metros))
+print('Os {:.1f}m que andaste corresponde a: \n{:.1f}mm\n{:.1f}cm\n{:.1f}dam\n{:.2f}hm\n{:.3f}km'.format(metros, metros*1000, metros*100, metros/10, metros/100, metros/1000))
